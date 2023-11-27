03da953e65d789e3ede30293286423da8f5f9f35
Shereemorphett <sheree.morphett@gmail.com>
2023-11-27 14:20:36 +0200

    removal of  unnecessary files etc

---
+++ src/WebServ/validateServers.cpp
@@ -0,0 +1,22 @@
+
+#include <vector>
+#include <map>
+#include <string> // check if needed
+#include <iostream>
+#include "WebServerProg.hpp"
+#include "../Color.hpp"
+// #include <cstdlib>
+
+
+    /*
+    Port numbers 0 - 1023 are used for well-known ports.
+    Port numbers 1024 - 65535 are available for the following user applications:
+    Port numbers 1024 - 49151 are reserved for user server applications.
+    Port numbers 49152 - 65535 are reserved for clients.
+    If you have a server in the client port range (49152 - 65535), define that server in the TCP/IP network services database to prevent port number conflicts.
+*/
+// void validateServers(const std::vector<struct server> &servers)
+// {
+
+
+// }
